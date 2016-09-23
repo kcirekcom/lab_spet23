b569e7afb7df1c46107ed1a95bf34de732d267e2
kcirekcom <erick.f.mock@gmail.com>
2016-09-23 16:09:50 -0700

    output for function of question 5 complete

--- app.js
+++ app.js
@@ -84,11 +84,13 @@ var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
 
+  var output = [sumArray2, message];
+  return output;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
